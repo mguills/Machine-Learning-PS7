a2c11838e80fb592ba17f8334f36d57ec0630c4b
mguills <mattjguil@gmail.com>
2018-03-23 17:02:28 -0700

    part a

--- source/soybean.py
+++ source/soybean.py
@@ -1,5 +1,5 @@
 """
-Author      : Yi-Chieh Wu
+Author      : Matt Guillory & Jackson Crewe
 Class       : HMC CS 158
 Date        : 2018 Mar 05
 Description : Multiclass Classification on Soybean Dataset
@@ -13,6 +13,8 @@ from util import *
 from sklearn.svm import SVC
 from sklearn import metrics
 
+import math
+
 ######################################################################
 # output code functions
 ######################################################################
@@ -20,12 +22,12 @@ from sklearn import metrics
 def generate_output_codes(num_classes, code_type) :
     """
     Generate output codes for multiclass classification.
-    
+
     For one-versus-all
         num_classifiers = num_classes
         Each binary task sets one class to +1 and the rest to -1.
         R is ordered so that the positive class is along the diagonal.
-    
+
     For one-versus-one
         num_classifiers = num_classes choose 2
         Each binary task sets one class to +1, another class to -1, and the rest to 0.
@@ -33,46 +35,62 @@ def generate_output_codes(num_classes, code_type) :
           the first class is positive and each following class is successively negative
           the second class is positive and each following class is successively negatie
           etc
-    
+
     Parameters
     --------------------
         num_classes     -- int, number of classes
         code_type       -- string, type of output code
                            allowable: 'ova', 'ovo'
-    
+
     Returns
     --------------------
         R               -- numpy array of shape (num_classes, num_classifiers),
                            output code
     """
-    
+
     ### ========== TODO : START ========== ###
     # part a: generate output codes
     # hint : initialize with np.ones(...) and np.zeros(...)
     R = None
+    if code_type == "ova":
+        R = np.ones((num_classes, num_classes))
+        for i in range(num_classes):
+            for j in range(num_classes):
+                if i != j:
+                    R[i][j] = -1
+    if code_type == "ovo":
+        num_classifiers = math.factorial(num_classes) / (math.factorial(2) * math.factorial(num_classes - 2))
+        R = np.zeros((num_classes, num_classifiers))
+        k = 0
+        for i in range(num_classes):
+            for j in range(num_classes-(i+1)):
+                R[i][k+j] = 1
+                R[i+j+1][k+j] = -1
+            k += num_classes - (i+1)
+
     ### ========== TODO : END ========== ###
-    
+
     return R
 
 
 def load_code(filename) :
     """
     Load code from file.
-    
+
     Parameters
     --------------------
         filename -- string, filename
     """
-    
+
     # determine filename
     import util
     dir = os.path.dirname(util.__file__)
     f = os.path.join(dir, '..', 'data', filename)
-    
+
     # load data
     with open(f, 'r') as fid :
         data = np.loadtxt(fid, delimiter=",")
-    
+
     return data
 
 
@@ -80,9 +98,9 @@ def test_output_codes():
     R_act = generate_output_codes(3, 'ova')
     R_exp = np.array([[  1, -1, -1],
                       [ -1,  1, -1],
-                      [ -1, -1,  1]])    
+                      [ -1, -1,  1]])
     assert (R_exp == R_act).all(), "'ova' incorrect"
-    
+
     R_act = generate_output_codes(3, 'ovo')
     R_exp = np.array([[  1,  1,  0],
                       [ -1,  0,  1],
@@ -97,11 +115,11 @@ def test_output_codes():
 def compute_losses(loss_type, R, discrim_func, alpha=2) :
     """
     Given output code and distances (for one example), compute losses (for each class).
-    
+
     hamming  : Loss  = (1 - sign(z)) / 2
     sigmoid  : Loss = 1 / (1 + exp(alpha * z))
     logistic : Loss = log(1 + exp(-alpha * z))
-    
+
     Parameters
     --------------------
         loss_type    -- string, loss function
@@ -111,36 +129,36 @@ def compute_losses(loss_type, R, discrim_func, alpha=2) :
         discrim_func -- numpy array of shape (num_classifiers,)
                         distance of sample to hyperplanes, one per classifier
         alpha        -- float, parameter for sigmoid and logistic functions
-    
+
     Returns
     --------------------
         losses       -- numpy array of shape (num_classes,), losses
     """
-    
+
     # element-wise multiplication of matrices of shape (num_classes, num_classifiers)
     # tiled matrix created from (vertically) repeating discrim_func num_classes times
     z = R * np.tile(discrim_func, (R.shape[0],1))    # element-wise
-    
+
     # compute losses in matrix form
     if loss_type == 'hamming' :
         losses = np.abs(1 - np.sign(z)) * 0.5
-    
+
     elif loss_type == 'sigmoid' :
         losses = 1./(1 + np.exp(alpha * z))
-    
+
     elif loss_type == 'logistic' :
         # compute in this way to avoid numerical issues
         # log(1 + exp(-alpha * z)) = -log(1 / (1 + exp(-alpha * z)))
         eps = np.spacing(1) # numpy spacing(1) = matlab eps
         val = 1./(1 + np.exp(-alpha * z))
         losses = -np.log(val + eps)
-    
+
     else :
         raise Exception("Error! Unknown loss function!")
-    
+
     # sum over losses of binary classifiers to determine loss for each class
     losses = np.sum(losses, 1) # sum over each row
-    
+
     return losses
 
 
@@ -170,11 +188,11 @@ def logistic_losses(R, discrim_func, alpha=2) :
 ######################################################################
 
 class MulticlassSVM :
-    
+
     def __init__(self, R, C=1.0, kernel='linear', **kwargs) :
         """
         Multiclass SVM.
-        
+
         Attributes
         --------------------
             R       -- numpy array of shape (num_classes, num_classifiers)
@@ -182,7 +200,7 @@ class MulticlassSVM :
             svms    -- list of length num_classifiers
                        binary classifiers, one for each column of R
             classes -- numpy array of shape (num_classes,) classes
-        
+
         Parameters
         --------------------
             R       -- numpy array of shape (num_classes, num_classifiers)
@@ -193,12 +211,12 @@ class MulticlassSVM :
                        see SVC documentation
             kwargs  -- additional named arguments to SVC
         """
-        
+
         num_classes, num_classifiers = R.shape
-        
+
         # store output code
         self.R = R
-        
+
         # use first value of C if dimension mismatch
         try :
             if len(C) != num_classifiers :
@@ -207,37 +225,37 @@ class MulticlassSVM :
                 C = np.ones((num_classifiers,)) * C[0]
         except :
             C = np.ones((num_classifiers,)) * C
-        
+
         # set up and store classifier corresponding to jth column of R
         self.svms = [None for _ in xrange(num_classifiers)]
         for j in xrange(num_classifiers) :
             svm = SVC(kernel=kernel, C=C[j], **kwargs)
             self.svms[j] = svm
-    
-    
+
+
     def fit(self, X, y) :
         """
         Learn the multiclass classifier (based on SVMs).
-        
+
         Parameters
         --------------------
             X    -- numpy array of shape (n,d), features
             y    -- numpy array of shape (n,), targets
-        
+
         Returns
         --------------------
             self -- an instance of self
         """
-        
+
         classes = np.unique(y)
         num_classes, num_classifiers = self.R.shape
         if len(classes) != num_classes :
             raise Exception('num_classes mismatched between R and data')
         self.classes = classes    # keep track for prediction
-        
+
         ### ========== TODO : START ========== ###
         # part c: train binary classifiers
-        
+
         # HERE IS ONE WAY (THERE MAY BE OTHER APPROACHES)
         #
         # keep two lists, pos_ndx and neg_ndx, that store indices
@@ -254,47 +272,47 @@ class MulticlassSVM :
         #     y_train should contain only {+1,-1}
         #
         # train the binary classifier
-        
+
         pass
         ### ========== TODO : END ========== ###
-    
-    
+
+
     def predict(self, X, loss_func=hamming_losses) :
         """
         Predict the optimal class.
-        
+
         Parameters
         --------------------
             X         -- numpy array of shape (n,d), features
             loss_func -- loss function
                          allowable: hamming_losses, logistic_losses, sigmoid_losses
-        
+
         Returns
         --------------------
             y         -- numpy array of shape (n,), predictions
         """
-        
+
         n,d = X.shape
         num_classes, num_classifiers = self.R.shape
-        
+
         # setup predictions
         y = np.zeros(n)
-        
+
         ### ========== TODO : START ========== ###
         # part d: predict multiclass class
-        # 
+        #
         # HERE IS ONE WAY (THERE MAY BE OTHER APPROACHES)
         #
         # for each example
         #   predict distances to hyperplanes using SVC.decision_function(...)
         #   find class with minimum loss (be sure to look up in self.classes)
-        # 
+        #
         # if you have a choice between multiple occurrences of the minimum values,
         # use the index corresponding to the first occurrence
-        
+
         pass
         ### ========== TODO : END ========== ###
-        
+
         return y
 
 
@@ -308,10 +326,11 @@ def main() :
     train_data = load_data("soybean_train.csv", converters)
     test_data = load_data("soybean_test.csv", converters)
     num_classes = 15
-    
+
     # part b : generate output codes
     test_output_codes()
-    
+    print generate_output_codes(5, 'ovo')
+
     ### ========== TODO : START ========== ###
     # parts c-e : train component classifiers, make predictions,
     #             compare output codes and loss functions
@@ -326,12 +345,12 @@ def main() :
     # if you implemented MulticlassSVM.fit(...) correctly,
     #   using OVA, your first trained binary classifier
     #   should have the following indices for support vectors
-    #     array([ 12,  22,  29,  37,  41,  44,  49,  55,  76, 134, 
+    #     array([ 12,  22,  29,  37,  41,  44,  49,  55,  76, 134,
     #            157, 161, 167, 168,   0,   3,   7])
     #
     # if you implemented MulticlassSVM.predict(...) correctly,
     #   using OVA and Hamming loss, you should find 54 errors
-    
+
     ### ========== TODO : END ========== ###
 
 if __name__ == "__main__" :
